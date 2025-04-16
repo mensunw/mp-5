286ce8a563be30aacec21b25d04a9ebcfea73117
Mensun <mensun@bu.edu>
2025-04-16 16:53:59 -0400

    init

---
+++ components/universal/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="flex items-center h-20">
+      {/* header */}
+      <h2 className="text-3xl font-semibold p-4">CS391 URL Shortener</h2>
+    </header>
+  )
+}
